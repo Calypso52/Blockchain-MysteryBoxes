c771df7feb670816abaf92f088c986cba2107286
AsterWei <aw3389@columbia.edu>
2022-05-12 17:24:10 -0400

    merged

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
-
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.4",
   paths: {
@@ -8,4 +10,12 @@ module.exports = {
     cache: "./src/backend/cache",
     tests: "./src/backend/test"
   },
+  defaultNetwork: "ropsten",
+  networks: {
+    hardhat: {},
+    ropsten: {
+       url: API_URL,
+       accounts: [`0x${PRIVATE_KEY}`]
+    }
+ },
 };
